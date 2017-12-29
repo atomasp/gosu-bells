7bca9feee38bebe742734a4acf689278079d4fc7
aitortomas <aitor.tomas@injixo.com>
2017-12-29 11:58:50 +0100

    Añadir fondo de pantalla

--- game.rb
+++ game.rb
@@ -5,12 +5,14 @@ class Game < Gosu::Window
   def initialize
     super 800, 600
     self.caption = "Juego de las campanas"
+    @background_image = Gosu::Image.new("media/bg.png", :tileable => true)
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end  
 end
 
