126d32bf3989cbe0f9f9c4b12ccca51f598ff4ec
aitortomas <aitor.tomas@injixo.com>
2017-12-29 16:34:57 +0100

    Añadir lógica para clickear en las campanas con el KB

--- game.rb
+++ game.rb
@@ -22,9 +22,43 @@ class Game < Gosu::Window
       @bells << Bell.new(self, position_x, 465, Gosu::Sample.new(tone))
     end
     @door = Door.new(self, 0, 0)
+    @bell_clicked = nil
+    @trigger = false
+    @start_time = 0
   end
 
   def update
+    if @trigger
+      if Gosu.milliseconds - @start_time < 100
+        @bell_clicked.click
+      else
+        @bells.each { |b| b.unclick }
+        @trigger = false
+      end
+    end
+  end
+
+  def button_down(key)
+    @start_time = Gosu.milliseconds
+    @trigger = true
+    case key
+    when Gosu::Kb1
+      @bell_clicked = @bells[0]
+    when Gosu::Kb2
+      @bell_clicked = @bells[1]
+    when Gosu::Kb3
+      @bell_clicked = @bells[2]
+    when Gosu::Kb4
+      @bell_clicked = @bells[3]
+    when Gosu::Kb5
+      @bell_clicked = @bells[4]
+    when Gosu::KbEscape
+      @trigger = false
+      close
+    else
+      @trigger = false
+      super
+    end
   end
 
   def draw
